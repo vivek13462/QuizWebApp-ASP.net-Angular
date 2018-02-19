d0ec4598767ebed3dbf75d31cc5cec932335947f
vivek.13462 <vivek.13462@gmail.com>
2018-02-19 12:33:32 -0800

    added seperate html page for question component

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {MatButtonModule} from '@angular/material';
+import {MatButtonModule, MatInputModule, MatCardModule} from '@angular/material';
 import { AppComponent } from './app.component';
 import { QuestionComponent } from './question.component';
 
@@ -10,7 +10,7 @@ import { QuestionComponent } from './question.component';
     AppComponent, QuestionComponent
   ],
   imports: [
-    BrowserModule, BrowserAnimationsModule, MatButtonModule
+    BrowserModule, BrowserAnimationsModule, MatButtonModule, MatInputModule, MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- frontend/src/app/question.component.ts
+++ frontend/src/app/question.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
     selector : 'question',
-    template : 'This is our Question <button mat-button>Click me!</button>'
+    templateUrl: './question.component.html'
 })
 
 export class QuestionComponent {
