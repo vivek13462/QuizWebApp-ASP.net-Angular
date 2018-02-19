efb7076170993dd4522b11926a6ea692b975a407
vivek.13462 <vivek.13462@gmail.com>
2018-02-19 12:04:44 -0800

    setup for using angular material

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatButtonModule} from '@angular/material';
 import { AppComponent } from './app.component';
 import { QuestionComponent } from './question.component';
 
@@ -10,7 +10,7 @@ import { QuestionComponent } from './question.component';
     AppComponent, QuestionComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, BrowserAnimationsModule, MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- frontend/src/app/question.component.ts
+++ frontend/src/app/question.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
     selector : 'question',
-    template : 'This is our Question'
+    template : 'This is our Question <button mat-button>Click me!</button>'
 })
 
 export class QuestionComponent {
